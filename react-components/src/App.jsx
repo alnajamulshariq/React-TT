55764ad98e6d9c3ba36357682ba65653046db0be
ShariqNajam <mschariquen@gmail.com>
2025-09-22 05:44:49 +0500

    reactComponents

---
+++ react-components/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card";
+
+function App(){
+  return(
+    <>
+    <h1 style={{textAlign:"center"}}>React App</h1>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ react-components/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <>
+    <div className="card">
+        <img src="qot.jpg" alt="qot.jpb" height="40%" width="40%"/>
+        <div className="cont">
+            <h2>Name: Queen Of Tears</h2>
+            <h3>Rating: 8.7</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos suscipit laborum officiis error culpa tempora eum magnam quaerat fugit odit?</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Card
