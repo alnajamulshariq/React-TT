6ad7003e6af3b52ac008aa50f715cc67709062b9
ShariqNajam <mschariquen@gmail.com>
2025-09-22 07:31:49 +0500

    conditional rendering in react

--- 3-conditional-rendering/src/App.jsx
+++ 3-conditional-rendering/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Card from './components/Card'
 
 function App() {
   return (
     <>
     <h1 style={{textAlign:"center"}}>Conditional Rendering</h1>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
     </>
   )
 }
--- 3-conditional-rendering/src/components/Card.jsx
+++ 3-conditional-rendering/src/components/Card.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
 
 function Card() {
+    const cardTitle = "Queen Of Tears";
+    const cardRating = 8.7;
+    const cardImage = "qot.jpg";
+    const cardDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos suscipit laborum officiis error culpa tempora eum magnam quaerat fugit odit?";
+    const age = 16;
+    const genreName = () => {
+        const genre = "Drama";
+        return genre;
+    }
+
+    // 1st tareeqa 
+    // if(age < 18){
+    //     return(
+    //     <div className="card">
+    //     <img src={cardImage} alt="qot.jpb" height="40%" width="40%"/>
+    //     <div className="cont">
+    //         <h2>Name: {cardTitle}</h2>
+    //         <h3>Rating: {cardRating}</h3>
+    //         <p>{cardDescription}</p>
+    //         <p>{genreName()}</p>
+    //         <button>Not Available</button>
+    //     </div>
+    // </div>
+    //     )
+    // }
+
+    // 3rd tareeqa
+    // let canWatch = "Not Available";
+    // if(age>=18){
+    //     canWatch = "Watch Now";
+    // }
+
+    // 4th tareeqa
+    const canWatch = () => {
+        if(age>=18){
+            return "Watch Now";
+        }
+        else{
+            return "Not Available";
+        }
+    }
+
+
+
   return (
     <>
-    
+    <div className="card">
+        <img src={cardImage} alt="qot.jpb" height="40%" width="40%"/>
+        <div className="cont">
+            <h2>Name: {cardTitle}</h2>
+            <h3>Rating: {cardRating}</h3>
+            <p>{cardDescription}</p>
+            <p>{genreName()}</p>
+            {/* 1st tareeqa */}
+            {/* <button>Watch Now</button> */}
+            {/* 2nd tareeqa */}
+            {/* <button>{age>=18?"Watch Now":"Not Available"}</button> */}
+            {/* 3rd tareeqa */}
+            {/* <button>{canWatch}</button> */}
+            {/* 4th tareeqa */}
+            <button>{canWatch()}</button>
+        </div>
+    </div>
     </>
   )
 }
