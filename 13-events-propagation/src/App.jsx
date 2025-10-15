becda89ea6c341e07b3bec992708fe2a6f6efa84
ShariqNajam <mschariquen@gmail.com>
2025-10-16 00:14:31 +0500

    event propagation

---
+++ 13-events-propagation/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { EventPropagation } from './EventPropagation';
+
+function App() {
+
+  return (
+    <>
+      <EventPropagation />
+    </>
+  )
+}
+
+export default App
